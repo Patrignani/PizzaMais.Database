8c01971821276520b3065eef9170b5e540354a19
andersonpatrignani <anderson.patrignani@gmail.com>
2021-05-02 21:46:21 -0300

    no message

--- src/PizzaMais.Database/dbo/Tables/Fornecedor.sql
+++ src/PizzaMais.Database/dbo/Tables/Fornecedor.sql
@@ -2,7 +2,8 @@
 (
 	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	[Nome] VARCHAR(80) NOT NULL,
-	[CNPJ] INT NOT NULL,
+	[Documento] VARCHAR(14) NOT NULL,
+	[TipoDocumento] SMALLINT,
 	[Ativo] BIT NOT NULL,
 	[DataAtualizacao] DATETIME NULL,
 	[DataCriacao] DATETIME NOT NULL,
